75a18a7c6fd04f599a9fc4afd639982f52790adb
coldsweat <coldsweat404@hotmail.com>
2017-12-17 16:20:09 +0100

    Creation de la socket, suivis du contexte d'adressage, compilation faite

--- client.c
+++ client.c
@@ -7,8 +7,28 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
+#define port 8084
+
 int main(int argc, char *argv[])
 {
+	int descripteur;
+	socklen_t taille;
+	struct sockaddr_in host;
+
+	//Creation de la socket, si cela echoue, quitter le programme
+
+	descripteur = socket(PF_INET, SOCK_STREAM, 0);
+	if(descripteur == -1)
+	{
+		perror("Creation de la socket impossible");
+		exit(EXIT_FAILURE);
+	}
+
+	//Creation du contexte d'adressage
+
+	host.sin_family = PF_INET;
+	host.sin_port = htons(port);
+	host.sin_addr.s_addr = inet_addr("127.0.0.1"); //Adress LocalHost
 
 	return EXIT_SUCCESS;
 }
